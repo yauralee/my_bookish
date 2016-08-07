aa6502564abc92084d7de0142b1b9965cee6d17a
liyu <1484084728@qq.com>
2016-08-07 13:15:59 +0800

    [#12] finish add book into my book list

--- app/controllers/my_books_controller.rb
+++ app/controllers/my_books_controller.rb
@@ -3,14 +3,23 @@ class MyBooksController < ApplicationController
 
 
   def create
-    p "##############"
-    p session[:current_user_id]
-    p "$$$$$$$$$$$$$$"
-    p params[:bookish_id]
-    @my_book = MyBook.new(user_id: session[:current_user_id], bookish_id: params[:bookish_id])
-    @my_book.save
     if logged_in?
+      @my_book = MyBook.new(user_id: session[:current_user_id], bookish_id: params[:bookish_id])
+      @my_book.save!
       redirect_to bookishes_path
+    else
+      redirect_to login_path
+    end
+  end
+
+  def index
+    if logged_in?
+      @my_books = MyBook.where(user_id: session[:current_user_id]).paginate(:page => params[:page]).order('id DESC')
+      @bookishes_in_my_books = []
+      @my_books.each do |my_book|
+        @bookishes_in_my_books.push(Bookish.find(my_book.bookish_id))
+      end
+      @bookishes_in_my_books
     else
       redirect_to login_path
     end
--- app/models/my_book.rb
+++ app/models/my_book.rb
@@ -1,4 +1,5 @@
 class MyBook < ActiveRecord::Base
   belongs_to :user
   belongs_to :bookish
+  self.per_page = 16
 end
